f0e17533f519708d72802c97cb57890ff9c63109
3313148636 <3313148636@qq.com>
2018-10-11 21:30:44 +0800

    project initial

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+
+
+import home_controller from './controllers/home_controller';
+
+home_controller.render();
---
+++ src/js/controllers/home_controller.js
@@ -0,0 +1,18 @@
+
+//home视图的控制器
+import home_template from '../views/home/home.html';
+
+// 负责将home视图模板渲染在对应的地方
+const render = () => {
+    console.log(1);
+    // 刚才说了，一切皆模块，说明html也是模块，看一下它暴露的是什么 发现是字符串
+    //console.log(home_template)
+    document.querySelector('#root').innerHTML = home_template;
+    
+}
+export default {
+    render
+}
+
+
+// 需求： 显示home  MVC
