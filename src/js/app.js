f3fbbbbb6d745e3164b1ea88e9a15483e9164d26
3313148636 <3313148636@qq.com>
2018-10-16 19:11:21 +0800

    '10/16'

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,5 @@
 
 
-// new Promise((resolve,reject)=>{
-//     home_controller.render();
-//     resolve();
-// }).then(()=>{
-//     shoplist_controller.render();
-// })
-
 import home_controller from './controllers/home_controller';
 home_controller.render(); 
+
--- src/js/controllers/shoplist_controller.js
+++ src/js/controllers/shoplist_controller.js
@@ -2,16 +2,15 @@
 
 import shoplist_template from '../views/shoplist/shoplist.html';
 import shoplist_models from '../models/shoplist_models.js';
-import BScroll from 'better-scroll';
 
 const render = async () => {
     
     let _template = Handlebars.compile(shoplist_template);
-
     var data = await shoplist_models.shop_list();
     let _html = _template({shoplist:data.items});
 
     $('ul.shoplist').html(_html); 
+    
     //查看活动
     $('li.item').bind('click',function(e){
         if(e.target.nodeName === 'I'){
@@ -30,15 +29,23 @@ const render = async () => {
         }
     })
 
-    let scroll = new BScroll('#root',{
-        probeType:2
-    })
-    //屏幕滚动事件
-    scroll.on('scroll',({x,y})=>{
-        if(y>0){
-            scroll.scrollTo(0,0);
+    var Y = $('#root').height(); 
+    var flag = true;
+    $(window).on('scroll',function(){
+        var scroll_y = $(window).scrollTop();
+        if(scroll_y > Y-1350 && flag == true){
+            flag = false;
+            $('.loading').css('display','flex');
+            setTimeout(()=>{
+                $('.loading').css('display','none');
+                $('ul.shoplist').html(_html+$('ul.shoplist').html());
+                Y = $('#root').height();
+                flag = true;
+            },2000)
         }
     })
+
+    
 }
 export default {
     render
